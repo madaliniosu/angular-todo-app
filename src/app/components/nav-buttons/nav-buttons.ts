4f7c5a5bc249a685237bf73ad194d4ae673afdc6
105gm <iosu.gm@gmail.com>
2026-02-26 11:57:37 +0100

    - styling - nav buttons styling

--- src/app/components/nav-buttons/nav-buttons.ts
+++ src/app/components/nav-buttons/nav-buttons.ts
@@ -1,21 +1,25 @@
 import { Component, inject, output } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { TodoStore } from '../../services/todo-store.service';
+import { MatIconModule } from '@angular/material/icon';
 
 export type Filter = 'All' | 'Open' | 'Completed';
 
 @Component({
   selector: 'app-nav-buttons',
-  imports: [MatListModule],
+  imports: [MatListModule, MatIconModule],
   templateUrl: './nav-buttons.html',
   styleUrl: './nav-buttons.css',
 })
 export class NavButtons {
   private store = inject(TodoStore);
-
+  activeFilter = this.store.filter; 
   buttons: Filter[] = ['All', 'Open', 'Completed'];
-
-  activeFilter = this.store.filter; // reactive binding
+  readonly filterIcons: Record<string, string> = {
+  'All': 'language',
+  'Open': 'rowing',
+  'Completed': 'done_all'
+};
 
   select(button: Filter) {
     this.store.setFilter(button);
